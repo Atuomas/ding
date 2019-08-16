def3b64731cc96b3da9c36a22ddd6f0ba7d19aa7
Atuomas <tuomas.antintupa@gmail.com>
2019-08-16 16:47:09 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app=express();
+var http = require('http').Server(app);
+var server = require('socket.io')(http);
+var port=8080;
+
+var counter=0;
+
+app.get('/', function(req, res) {
+
+        res.sendFile(__dirname + '/index.html');
+    });
+
+
+server.on('connection', function(socket)
+{
+    console.log('a user connected');
+
+    socket.emit('click_count', counter);
+
+    socket.on('clicked',function(){
+    counter+=1;
+
+    server.emit('click_count', counter);
+    });
+
+});
+
+http.listen(port, function()
+{
+    console.log('listening on port:'+port);
+});
